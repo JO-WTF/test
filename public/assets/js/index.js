804237c0fdd1d2d085786853fcd95c3da144f9b7
Zhaoyu Zhong <zhongzhaoyu1@huawei.com>
2025-09-17 23:16:43 +0700

    key

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -48,20 +48,21 @@ let currentStream = null;
 let scanner;
 
 const sdk = await ScanbotSDK.initialize({
-  licenseKey: "Qgw4fb+KJrZwDqjNSnLAm8mbj5YSf5" +
-  "pmApU/SJgTlx3n5ipAW3sFPuSlLMnG" +
-  "Zc+tPJ9A42AWACI4PqLYI+lJcSl+Tf" +
-  "+eKIN6Cgg+KY9fbcL4cZ/twiHfXCTC" +
-  "G+TVnAzV01oXtFph1OB0eRbQayycEn" +
-  "asKcz2aCwzZOQiXXQFGhQljolRqocc" +
-  "HLUi7L16Rebk8+yoh8ClxsTM+ruiXf" +
-  "BwVZJqcsKd+Okl3g2sIC99BMuzQ8WO" +
-  "GOUqscDcUddDK6fHXfpnSDWt3ckkbD" +
-  "QmD+ODQ3qt1IRAVxRGkbaspMpnge81" +
-  "aCqiU/w6SL+GoN+4V+i84WLUomS6i1" +
-  "uWBUic83NFEQ==\nU2NhbmJvdFNESw" +
-  "psb2NhbGhvc3R8ZHBkbnMub3JnCjE3" +
-  "NTg3NTgzOTkKODM4ODYwNwo4\n", // Empty string is the default value for trial mode
+  licenseKey: "KH1nmhmptRSxGqMQ5BhSpleutwfhXC" +
+  "eOcDnM2KfTF6s46RYW1En5HCWXlyIU" +
+  "+KS10sRn56VZu93HfAyQAbmXgRfa9v" +
+  "IdpdqaTs3ZtfXCXYyDdVq/pdimdtky" +
+  "Ykcj7sLQzj9BAUhYRsgepbBzlpj4ao" +
+  "9G9KkIzEL+X4GLtpg2kiqnK/J+wmx5" +
+  "3iqaCqscIE5EyqZAvZKVbHpNlfqt8h" +
+  "TIM5Jpot0jj0A3kCNl1IhhM7a/2w3l" +
+  "g/cD3jwJy/AcRkobKeq42dsJt9Vq9/" +
+  "GMtLlO2iuZrp1v62yXEnFL5vFOnukW" +
+  "EtC/ARPl/5nsXk067Htwz7YI7rqd3Y" +
+  "QWnUuaAl9LPA==\nU2NhbmJvdFNESw" +
+  "psb2NhbGhvc3R8aWRuc2MuZHBkbnMu" +
+  "b3JnCjE3NTg3NTgzOTkKODM4ODYwNw" +
+  "o4\n", // Empty string is the default value for trial mode
   enginePath: "https://cdn.jsdelivr.net/npm/scanbot-web-sdk@7.2.0/bundle/bin/complete/"
 });
 const configuration = {
