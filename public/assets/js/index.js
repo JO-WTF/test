1bed48393c40102780aedfd9361d3e6419fe56de
Zhaoyu Zhong <zhongzhaoyu1@huawei.com>
2025-09-17 12:35:38 +0700

    ffff

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -401,7 +401,7 @@ const app = createApp({
           remark: state.remark,
           photo: state.photoPreview || null,
           lng: state.location?.lng,
-          lat: state.location?.lat,,
+          lat: state.location?.lat,
         };
         state.showResult = true;
         state.last = true;
